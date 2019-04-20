fbd73ea4a4c11d2a8497969f0328811a37686f15
MrAugu <mraugu@yahoo.com>
2019-04-20 11:16:53 +0300

    CircleKMS [2]

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
     "callbackURL": "/callback",
     "sessionSecret": "dfd$##$%$#%$CFdfdf4%$%#@#$@@##@$#@EncryptionIsForce%^%53345#$%$#%22334+324238+294423+4e2*/1e+dsafqerq352/4+",
     "domain": "localhost",
-    "port": 80
+    "port": 3001
   },
   "dbUrl": "mongodb://system:system~1@ds012889.mlab.com:12889/discord-house",
   "admins": ["414764511489294347", "538855659714641960"]
