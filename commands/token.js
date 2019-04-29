41204c866ee620e665e2567aafd2a0d1162e65b3
Christopher KHAWAND <cwkhawand@gmail.com>
2019-04-29 21:09:47 +0300

    Update token.js
    Grammar

--- commands/token.js
+++ commands/token.js
@@ -11,23 +11,23 @@ function makeid(length) {
 }
 
 module.exports.run = async (client, message, args, reply) => {
-  if (!args[0]) return reply("You have to specify an option. The valid ones are `show`,  `regenerate` or `generate`.");;
+  if (!args[0]) return reply("<:redTick:568885082321059865> Please specify an option. \nThe valid options are are: `show`,  `regenerate` or `generate`.");;
   var bot = message.mentions.users.first() || { id: args[1] };
   if (bot) bot = bot.id;
-  if (!bot) return reply("Please specify a bot.");
+  if (!bot) return reply("<:redTick:568885082321059865> Please specify a bot.");
 
   var userBots = await require("../models/bots.js").find();
-  if (userBots.findIndex(b => b.id === bot) < 0) return reply("That bot is not listed.");
+  if (userBots.findIndex(b => b.id === bot) < 0) return reply("<:redTick:568885082321059865> The specified bot is not listed.");
   userBots = userBots.filter(b => b.mainOwner === message.author.id);
   if (userBots.length < 1) return reply("You don't have any bots listed.");
-  if (userBots.findIndex(b => b.id === bot) < 0) return reply("You don't own acces to specified bot.");
+  if (userBots.findIndex(b => b.id === bot) < 0) return reply("<:redTick:568885082321059865> You do not own the specified bot.");
 
   if (args[0].toLowerCase() === "show") {
     const botIndex = userBots.findIndex(b => b.id === bot);
     const theBot = userBots[botIndex];
-    if (theBot.token === null) return reply("Please generate a token using this command's `generate` option.");
-    message.author.send(`The token for this bot is \`${theBot.token}\`.`);
-    reply("Token has been DMed to you.");
+    if (theBot.token === null) return reply("<:redTick:568885082321059865> Please generate a token using this command's `generate` option.");
+    message.author.send(`The token attached to this bot is \`${theBot.token}\`.`);
+    reply("<:greenTick:568885198519926784> The token has been DMed to you.");
   } else if (args[0].toLowerCase() === "generate") {
     const botIndex = userBots.findIndex(b => b.id === bot);
     const theBot = userBots[botIndex];
@@ -35,7 +35,7 @@ module.exports.run = async (client, message, args, reply) => {
       const newToken = `XA-${makeid(12)}`;
       bott.token = newToken;
       await bott.save().catch(e => console.log(e));
-      reply("Token has been generated for this bot. Please use this command's `show` option to view it.");
+      reply("<:greenTick:568885198519926784> Token has been generated for this bot. Please use this command's `show` option to view it.");
     });
   } else if (args[0].toLowerCase() === "regenerate") {
     const botIndex = userBots.findIndex(b => b.id === bot);
@@ -44,10 +44,10 @@ module.exports.run = async (client, message, args, reply) => {
       const newToken = `XA-${makeid(12)}`;
       bott.token = newToken;
       await bott.save().catch(e => console.log(e));
-      reply("Token has been regenerated for this bot. Please use this command's `show` option to view it.");
+      reply("<:greenTick:568885198519926784> Token has been regenerated for this bot. Please use this command's `show` option to view it.");
     });
   } else {
-    reply("You have to specify am option. The valid ones are `show`,  `regenerate` or `generate`.");
+    reply("<:redTick:568885082321059865> Please specify an option. \nThe valid options are: `show`,  `regenerate` or `generate`.");
   }
 };
 
