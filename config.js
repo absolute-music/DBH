42788370379d84322675f313eeb2e8043137405d
MrAugu <39545629+MrAugu@users.noreply.github.com>
2019-09-08 17:57:01 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "token": "BOT",
+  "dashboard": {
+    "oauthSecret": "CLIENT_SECRET",
+    "callbackURL": "http://localhost/callback", // add this to OAuth page of your application
+    "sessionSecret": "dfd$##$%$#%$CFdfdf4%$%#@#$@@##@$#@EncryptionIsForce%^%53345#$%$#%22334+324238+294423+4e2*/1e+dsafqerq352/4+",
+    "domain": "localhost",
+    "port": 80
+  },
+  "dbUrl": "MONGO_DATABASE_URL",
+  "admins": []
+};
