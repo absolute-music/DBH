ad6a4c41a8a6feee1ec30bc8cbd9530c1550d69f
MrAugu <39545629+MrAugu@users.noreply.github.com>
2019-09-09 06:41:03 +0300

    200iq text patched

--- commands/eval.js
+++ commands/eval.js
@@ -15,7 +15,7 @@ const code = args.join(" ");
           let ev = require('util').inspect(eval(code), { depth: 0 });
           let token = client.token.replace(/\./g, "\.")
           let re = new RegExp(token, 'g') 
-          ev = ev.replace(re, "NO.FUCKING.TOKEN-f4DF5-4U-LOL-GET-GNOMED.54d4FFW.LOL").replace(client.config.dbUrl, "PornHub:Sex:67hdk3@ds067781.pornhub.com:5702/hot");
+          ev = ev.replace(re, "NO.FUCKING.TOKEN-f4DF5-4U-LOL-GET-GNOMED.54d4FFW.LOL").replace(client.config.dbUrl, "mongodb://this:was.supposed@to/?be.a.db.url.aint.it");
           const cleaned =  clean(ev)
           const MAX_CHARS = 3 + 2 + cleaned.length + 3;
           if (MAX_CHARS > 1500) {
